a52e4b7192e9811129466f12684a6a709d2f34d9
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-01-17 16:27:59 -0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cp /package-lock.json /github/workspace
 cp /package.json /github/workspace
 
 npm install
-node store.js $1 $2
+node store.js $1 $2 $3
 
 if [ $? != 0 ]; then
   echo "Execution error"
